6b84dc24f43d67b68c04e1b80f0657869dde59c9
gabriellaMT <gabriellamichelle752@gmail.com>
2015-03-11 01:48:30 +0800

    update pickmove method + extends class

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -2,13 +2,28 @@
 public class PlayerSkeleton {
 
 	//implement this function to have a working system
-	public int pickMove(State s, int[][] legalMoves) {
+	public int pickMove(Game s, int[][] legalMoves) {
+		Strategy strategy = new Strategy();
+		double maxScore = -1;
+		int move = 0;
+		Game simulation;
 		
-		return 0;
+		for(int i = 0; i < legalMoves.length; i++) {
+			simulation = (Game) s.clone();
+			simulation.makeMove(i);
+			double score = simulation.getRowsCleared() + strategy.calculate(simulation);
+			
+			if(score > maxScore) {
+				maxScore = score;
+				move = i;
+			}
+		}
+		
+		return move;
 	}
 	
 	public static void main(String[] args) {
-		State s = new State();
+		Game s = new Game();
 		new TFrame(s);
 		PlayerSkeleton p = new PlayerSkeleton();
 		while(!s.hasLost()) {
@@ -22,6 +37,22 @@ public class PlayerSkeleton {
 			}
 		}
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
+	}
+	
+}
+
+class Game extends State implements Cloneable {
+	
+	public Game() {
+		super();
+	}
+	
+	public Object clone(){  
+	    try{  
+	        return super.clone();  
+	    }catch(Exception e){ 
+	        return null; 
+	    }
 	}
 	
 }
