a22b8dd08787471c78f12d72f5fe2d8a5ff392a1
gabriellaMT <gabriellamichelle752@gmail.com>
2015-03-11 01:49:35 +0800

    Revert "update pickmove method + extends class"
    This reverts commit 6b84dc24f43d67b68c04e1b80f0657869dde59c9.

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -2,28 +2,13 @@
 public class PlayerSkeleton {
 
 	//implement this function to have a working system
-	public int pickMove(Game s, int[][] legalMoves) {
-		Strategy strategy = new Strategy();
-		double maxScore = -1;
-		int move = 0;
-		Game simulation;
+	public int pickMove(State s, int[][] legalMoves) {
 		
-		for(int i = 0; i < legalMoves.length; i++) {
-			simulation = (Game) s.clone();
-			simulation.makeMove(i);
-			double score = simulation.getRowsCleared() + strategy.calculate(simulation);
-			
-			if(score > maxScore) {
-				maxScore = score;
-				move = i;
-			}
-		}
-		
-		return move;
+		return 0;
 	}
 	
 	public static void main(String[] args) {
-		Game s = new Game();
+		State s = new State();
 		new TFrame(s);
 		PlayerSkeleton p = new PlayerSkeleton();
 		while(!s.hasLost()) {
@@ -37,22 +22,6 @@ public class PlayerSkeleton {
 			}
 		}
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
-	}
-	
-}
-
-class Game extends State implements Cloneable {
-	
-	public Game() {
-		super();
-	}
-	
-	public Object clone(){  
-	    try{  
-	        return super.clone();  
-	    }catch(Exception e){ 
-	        return null; 
-	    }
 	}
 	
 }
