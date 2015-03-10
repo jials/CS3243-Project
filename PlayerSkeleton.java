4246ea2f9ffc4e1379d81ea554dfefcee80c41c3
gabriellaMT <gabriellamichelle752@gmail.com>
2015-03-11 02:03:30 +0800

    update pickmove

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -2,13 +2,28 @@
 public class PlayerSkeleton {
 
 	//implement this function to have a working system
-	public int pickMove(State s, int[][] legalMoves) {
+	public int pickMove(Game s, int[][] legalMoves) {
+		Strategy strategy = new Strategy();
+		int move = 0;
+		double maxScore = -1;
+		Game simulation;
 		
-		return 0;
+		for(int i = 0; i < legalMoves.length; i++) {
+			simulation = (Game) s.clone();
+			simulation.makeMove(i);
+			double score = simulation.getRowsCleared() + strategy.calculate(simulation);
+			
+			if(score > maxScore) {
+				maxScore = score;
+				move = i;
+			}
+		}
+		
+		return move;
 	}
 	
 	public static void main(String[] args) {
-		State s = new State();
+		Game s = new Game();
 		new TFrame(s);
 		PlayerSkeleton p = new PlayerSkeleton();
 		while(!s.hasLost()) {
