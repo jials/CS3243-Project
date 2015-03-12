2d10bffc5f4ef7d1e7571566bfb7c4afbed74be6
Diga Widyaprana <digawp@gmail.com>
2015-03-12 18:05:46 +0800

    Implement getScore

--- Learning.java
+++ Learning.java
@@ -1,10 +1,38 @@
+abstract class Learning {
 
-public abstract class Learning {
-	protected double getScore(Strategy strategy){
-		return 0.0;
+	/**
+	 * Run a game of tetris using the passed strategy
+	 * @param strategy
+	 * @return the number of rows cleared
+	 */
+	protected int getScore(Strategy strategy){
+		State s = new State();
+		while(!s.hasLost()) {
+			s.makeMove(pickMove(s, s.legalMoves(), strategy));
+		}
+		return s.getRowsCleared();
 	}
-	
+
+	private int pickMove(State s, int[][] legalMoves, Strategy strategy) {
+		int move = 0;
+		double maxScore = -1;
+		Game simulation;
+
+		for(int i = 0; i < legalMoves.length; i++) {
+			simulation = new Game(s);
+			simulation.makeMove(i);
+			double score =
+					simulation.getRowsCleared() + strategy.calculate(simulation);
+
+			if(score > maxScore) {
+				maxScore = score;
+				move = i;
+			}
+		}
+		return move;
+	}
+
 	protected abstract void store();
-	
+
 	protected abstract void load();
 }
