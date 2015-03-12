ec1a7d42b84cdb417db13f9f6b5d372b90fd17df
Diga Widyaprana <digawp@gmail.com>
2015-03-12 23:30:51 +0800

    Fix formatting, change return type of load to boolean

--- Learning.java
+++ Learning.java
@@ -6,9 +6,9 @@ abstract class Learning {
 	 * @param strategy
 	 * @return the number of rows cleared
 	 */
-	protected int getScore(Strategy strategy){
+	protected int getScore(Strategy strategy) {
 		State s = new State();
-		while(!s.hasLost()) {
+		while (!s.hasLost()) {
 			s.makeMove(pickMove(s, s.legalMoves(), strategy));
 		}
 		return s.getRowsCleared();
@@ -18,23 +18,26 @@ abstract class Learning {
 	 * Run simulations for all the next legal move and pick the one that will
 	 * give the best possible score according to the strategy
 	 *
-	 * @param s the current state of the game =
+	 * @param s
+	 *            the current state of the game
 	 * @param legalMoves
-	 * @param strategy the strategy being used
-	 * @return the index of the "best" move (supposedly...)
+	 * @param strategy
+	 *            the strategy being used
+	 * @return the index of the move with highest score according to the
+	 *         strategy
 	 */
 	private int pickMove(State s, int[][] legalMoves, Strategy strategy) {
 		int move = 0;
 		double maxScore = -1;
 		Game simulation;
 
-		for(int i = 0; i < legalMoves.length; i++) {
+		for (int i = 0; i < legalMoves.length; i++) {
 			simulation = new Game(s);
 			simulation.makeMove(i);
-			double score =
-					simulation.getRowsCleared() + strategy.calculate(simulation);
+			double score = simulation.getRowsCleared()
+					+ strategy.calculate(simulation);
 
-			if(score > maxScore) {
+			if (score > maxScore) {
 				maxScore = score;
 				move = i;
 			}
@@ -50,5 +53,5 @@ abstract class Learning {
 	/**
 	 * Load the result of previous run
 	 */
-	protected abstract void load();
+	protected abstract boolean load();
 }
