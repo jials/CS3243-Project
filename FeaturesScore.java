76b58b02610f0f8f6c26d4f1aadef20e79f8963d
nathanajah <nathanajah@yahoo.com>
2015-04-03 22:13:13 +0800

    Fixed bug (wrong number of rows)

--- FeaturesScore.java
+++ FeaturesScore.java
@@ -1,7 +1,7 @@
 
 public class FeaturesScore {
 	public static final int COLS = 10;
-	public static final int ROWS = 19;
+	public static final int ROWS = 20;
 
 	//currently 21 features. To be increased in the future
 	public static final int NUM_FEATURES = 6;
