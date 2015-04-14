666e56c28f96342943e58493020a2e3573e682f5
Diga Widyaprana <digawp@gmail.com>
2015-04-14 21:17:43 +0800

    Bugfixes, mixed up between cols and rows

--- FeaturesScore.java
+++ FeaturesScore.java
@@ -186,7 +186,7 @@ public class FeaturesScore {
 		int count = 0;
 		for (int i = 1; i < Game.COLS; i++) {
 			for (int j = 0; j < Game.ROWS; j++) {
-				if (grid[i][j] > 0 && grid[i][j-1] == 0) {
+				if (grid[j][i] > 0 && grid[j][i-1] == 0) {
 					count++;
 				}
 			}
@@ -202,9 +202,9 @@ public class FeaturesScore {
 	 */
 	private int getSumHoleDepth(int[][] grid) {
 		int count = 0;
-		for (int i = 0; i < Game.COLS; i++) {
+		for (int i = 0; i < Game.COLS - 1; i++) {
 			for (int j = 0; j < Game.ROWS; j++) {
-				if (grid[i][j] == 0 && grid[i][j+1] > 0) {
+				if (grid[j][i] == 0 && grid[j][i+1] > 0) {
 					count += getHoleDepth(grid, i, j);
 				}
 			}
